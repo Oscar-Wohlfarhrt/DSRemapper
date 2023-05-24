156f9688c5ca8eab3eedcdb172d917fa1925b7ad
Oscar-Wohlfarhrt <oscarwohlfarhrt@gmail.com>
2023-05-23 21:55:44 -0300

    RemapperCore Update

--- DSRemapper.RemapperCore/RemapperCore.cs
+++ DSRemapper.RemapperCore/RemapperCore.cs
@@ -12,12 +12,49 @@ namespace DSRemapper.RemapperCore
         private readonly IDSInputController controller;
         private readonly IDSRemapper remapper;
         private readonly Thread thread;
+        private readonly CancellationToken cancellationToken;
+        //Timer timer;
 
         public Remapper(IDSInputController controller, IDSRemapper remapper)
         {
             this.controller = controller;
             this.remapper = remapper;
-            thread = new();
+            cancellationToken = new CancellationToken();
+
+            /*timer = new Timer(RemapTimer);
+            timer.Change(0,10);*/
+            
+            thread = new(RemapThread)
+            {
+                Name=$"{controller.ControllerName} Remapper",
+                Priority = ThreadPriority.Normal
+            };
+
+            /*Task.Factory.StartNew(RemapThread, CancellationToken.None,
+                TaskCreationOptions.LongRunning, TaskScheduler.Default);*/
+        }
+
+        public void Start()
+        {
+            thread.Start();
+        }
+
+        public void Stop()
+        {
+            
+        }
+
+        private void RemapThread()
+        {
+            while (cancellationToken.IsCancellationRequested)
+            {
+
+                Thread.Sleep(10);
+            }
+        }
+        private void RemapTimer(object? sender)
+        {
+
         }
     }
 }
