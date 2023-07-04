6d49909d4624614ce077065ed8703f993e92cfc8
Oscar-Wohlfarhrt <59288986+Oscar-Wohlfarhrt@users.noreply.github.com>
2023-07-03 21:25:32 -0300

    Delete build directory

--- build/publish/wwwroot/js/jsinterop.js
+++
@@ -1,9 +0,0 @@
-﻿function ScrollToBottom(ele){
-    ele.scrollTop=ele.scrollHeight;
-}
-function AddHTML(ele, text){
-    ele.innerHTML+=text;
-}
-function SetHTML(ele, text){
-    ele.innerHTML=text;
-}
